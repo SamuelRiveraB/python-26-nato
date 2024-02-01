a15ae06fbcfb5454db3f025c0ed29212d72f52e3
Samuel <samuelrivba@gmail.com>
2024-02-01 14:46:04 -0500

    Add error catching

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 alpha_dict = {row.letter: row.code for (index, row) in alphabet.iterrows()}
 print(alpha_dict)
 
-word = input("Enter a word: ").upper()
-nato_list = [alpha_dict[i] for i in word]
-print(nato_list)
+
+def gen():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_list = [alpha_dict[i] for i in word]
+    except KeyError:
+        print("Error: Only letters allowed")
+        gen()
+    else:
+        print(nato_list)
+
+
+gen()
